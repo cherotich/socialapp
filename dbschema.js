6981b2724071034eb3d130b15410c69389e67973
cherotich <carolcaro60@gmail.com>
2020-04-25 01:22:50 +0300

    intitial commit

---
+++ dbschema.js
@@ -0,0 +1,13 @@
+let db ={
+    screams:
+    [{
+
+    
+        userHandle: 'user',
+        body: 'this is the scream body',
+        createdAt: '2020-04-22T17:07:02.022Z',
+        likeCount: 5,
+        commentCount :3
+    }
+    ]
+}
