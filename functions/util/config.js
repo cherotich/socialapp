2c91aaf825cba3175bdbe949d09be8101cc89127
cherotich <carolcaro60@gmail.com>
2020-04-28 20:25:48 +0300

    refacturing and restructure

---
+++ functions/util/config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    apiKey: "AIzaSyDlcU3tb-4he9C0fkUg8o5apzK4scYgANw",
+    authDomain: "socialapp-8f164.firebaseapp.com",
+    databaseURL: "https://socialapp-8f164.firebaseio.com",
+    projectId: "socialapp-8f164",
+    storageBucket: "socialapp-8f164.appspot.com",
+    messagingSenderId: "19174856358",
+    appId: "1:19174856358:web:9aa22d7a9f7fa67e270667",
+    measurementId: "G-VHFMN9MK49"
+  };
